71f1840270b061d0d3e53a69f738eb240a2395ba
ZeinyLing <111239852+ZeinyLing@users.noreply.github.com>
2026-06-10 17:56:03 +0800

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -4,14 +4,11 @@ import numpy as np
 ens = pd.read_csv("./ensemble_clean0.50_gap0.50.csv")
 
 params = {
-    "pred_week1": {"a": 0.7, "b": 0.5, "t": 0.5},
-    "pred_week2": {"a": 0.8, "b": 0.4, "t": 0.5},
-    #"pred_week1": {"a": 0.65, "b": 0.45, "t": 0.5},
-    #"pred_week1": {"a": 0.7706107571, "b": 0.5396658440, "t": 0.555},
-    #"pred_week2": {"a": 0.8089307278, "b": 0.4456145607, "t": 0.530},
-    #"pred_week3": {"a": 0.8195200042, "b": 0.3840128595, "t": 0.750},
-    #"pred_week4": {"a": 0.8466602880, "b": 0.3576618927, "t": 0.810},
-    #"pred_week5": {"a": 0.8595354424, "b": 0.3133853840, "t": 0.765},
+    "pred_week1": {"a": 0.7, "b": 0.5, "t": 0.55},
+    "pred_week2": {"a": 0.8, "b": 0.4, "t": 0.53},
+    "pred_week3": {"a": 0.8, "b": 0.3, "t": 0.75},
+    "pred_week4": {"a": 0.8, "b": 0.3, "t": 0.81},
+    "pred_week5": {"a": 0.8, "b": 0.3, "t": 0.76},
 }
 
 out = ens.copy()
@@ -23,4 +20,4 @@ for col, p in params.items():
     pred[pred < p["t"]] = 0
     out[col] = pred
 
-out.to_csv("ensemble_calibrated_affine_zero_w1_1.csv", index=False)
+out.to_csv("ensemble_v4.csv", index=False)
